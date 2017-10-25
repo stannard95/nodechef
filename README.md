736ed1103c9937cdfa4101d3b2b4c2e65ee68304
stannard95 <14066588@brookes.ac.uk>
2017-10-25 12:00:35 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# node-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: node-server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
